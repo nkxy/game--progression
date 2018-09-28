551e97fb45446017f4aff7bd7b9dcf9c8a1af30c
Nick Yang <nick.yang@rangle.io>
2018-09-28 11:39:38 -0400

    :recycle: refactored routing into modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { GamesComponent } from './components/games/games.component';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
 import { DashboardComponent } from './modules/dashboard/components/dashboard/dashboard.component';
+import { ProfileComponent } from './modules/profile/components/profile/profile.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
   { path: 'games', component: GamesComponent },
   { path: '**', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
